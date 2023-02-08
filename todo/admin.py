49cb3126d69ec7081a8fc577f9594a9be372eae2
Maurizio <maurizio.loffredo@outlook.com>
2023-02-08 22:57:56 +0000

    Feat: registered first model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+# Even though our tables have been created upon migration, and we could start
+# creating items programmatically, we will not be able to see our items into
+# admin until we expose them. To do that, we need to register our model here.
+
+# Register your models here:
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+# When Django sees that we have created a new class, it will automatically
+# create a related table when we make and run the database migrations.
+
+# By themselves, our defined classes will do nothing. We need to use something
+# called class inheritance to give them some functionality. Here, we inherit
+# the base model class by putting models.model in the parentheses. In this way,
+# our defined class can do everything the built-in Django model class can do.
+
+# All that is left to do is define the attributes that our class will have.
+# We can skip the 'id' field, since Django will create it for us automatically.
+
+
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # By default, all models that inherit this base model class will use its
+    # built-in string method to display their class name, followed by the word
+    # object. To change that, we need to override that string method with our
+    # own, i.e. we need to redefine it here, in our own class. Therefore, it
+    # is going to return the item class's name attribute, which is the name
+    # that we put into the admin form online:
+    def __str__(self):
+        return self.name
